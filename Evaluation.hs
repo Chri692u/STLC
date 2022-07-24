2edacd564c4a4c034e5d5b9bdc7394b140e276da
Christoffer <chri692u@gmail.com>
2022-07-24 17:42:02 +0200

    clone clean

--- Evaluation.hs
+++ Evaluation.hs
@@ -1,23 +1,4 @@
 module Evaluation where
--- AST
-type Id = String
-
-data Expr = Inum Int
-          | Let Id Expr Expr
-          | Var Id
-          | Lambda Id Expr
-          | App Expr Expr
-          | Binary Operation Expr Expr
-          deriving (Show)
-
-data Operation = Add
-           deriving (Show)
-           
-data Value = Ilit Int
-           | Closure Id Expr Env
-           deriving (Show)
-
-type Env = [(Id, Value)]
 
 -- Evaluation of literals, variables, let-in bindings, lambda abstractions and lambda application
 eval :: Expr -> Env -> Value
